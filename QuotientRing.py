23b15c48bc2d0035a6aa4446346de30cbc173f9a
Robert Knauer <robert@privatdemail.net>
2015-07-24 20:27:01 +0200

    added square & multiply

--- QuotientRing.py
+++ QuotientRing.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
 
 class Helpers(object):
-	pass
+	@staticmethod
+	def squareAndMultiply(b, e, n): ##FIXME##
+		##TODO: Euler's theorem
+		mask = 2**e.bit_length()
+		hadFirstOne = False
+		res = 1
+		for i in range(e.bit_length(), -1, -1):
+			if (e & mask) == 0:
+				if hadFirstOne:
+					res = (res**2) % n
+			else:
+				res = (res**2) % n
+				res = (res*b) % n
+				hadFirstOne = True
+			mask >>= 1
+		return res
--- QuotientRingTest.py
+++ QuotientRingTest.py
@@ -3,4 +3,7 @@ from QuotientRing import Helpers
 import unittest
 
 class HelpersTest(unittest.TestCase):
-	pass
+	def testSquareAndMultiply(self): ##TODO##
+		testValues = { (3,201,100):3 }
+		for val in testValues.items():
+			self.assertEqual(Helpers.squareAndMultiply(val[0][0], val[0][1], val[0][2]), val[1], "Failed for (b,e,n) = (%d,%d,%d)" % val[0])
